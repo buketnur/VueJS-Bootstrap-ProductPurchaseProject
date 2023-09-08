f184a96670d2d656a382899eaa8fffa9d8b1b3a0
Buket Nur Astan <buketnurertas@gmail.com>
2023-09-09 02:08:28 +0300

    initial

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <app-header></app-header>
+  <transition name = "fade" mode = "out-in">
+    <router-view></router-view>
+  </transition>
+  <app-footer></app-footer>
+</div>
+</template>
+<script>
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+
+export default {
+  components :{
+    appHeader : Header,
+    appFooter : Footer
+  },
+  created(){
+    this.$store.dispatch("getTradeResult");
+    this.$store.dispatch("initApp");
+  }
+}
+</script>
+<style>
+body{
+  background-color: #f2fcf8;
+}
+.fade-enter{
+  opacity : 0,
+}
+.fade-enter-active{
+  transition: opacity .5s ease-out;
+}
+/* .fade-leave{} */
+.fade-leave-active{
+  transition: opacity .5s ease-out;
+  opacity : 0;
+}
+</style>
---
+++ components/shared/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar fixed-bottom  navbar-light" style="background-color: #def1eb;">
+    <span class="navbar-text mr-auto">
+        <strong>vuejsegitim.com</strong> | buketnurastan
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+        <strong>Alış Tutarı : </strong>
+        <span class="badge badge-danger" style="opacity:0.5;">{{getTradeResult.purchase | currency}}</span>
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
+        <strong>Satış Tutarı : </strong>
+        <span class="badge badge-success" style="opacity:0.5">{{getTradeResult.sale | currency}}</span>
+    </span>
+    <span class="navbar-text navbar-nav my-2 my-lg-0">
+        <strong>Bakiye : </strong>
+        <span class="badge badge-primary" style="opacity:0.5">{{getTradeResult.balance | currency}}</span>
+    </span>
+</nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default{
+    computed : mapGetters(["getTradeResult"]),
+
+}
+
+</script>
